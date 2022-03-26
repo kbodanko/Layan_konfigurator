8b4f9e40fc03a9c83c85308aa814a7f2a9a3c954
kbodanko <katarzyna.bodanko@gmail.com>
2022-03-26 14:21:11 +0100

    amendments

--- konfigurator.js
+++ konfigurator.js
@@ -11,21 +11,31 @@ let filtersBody = document.querySelector('.filters');
 let isDropDown = false;
 let productCards = document.querySelectorAll('.product_card');
 let backIcon = document.querySelector('.back_icon_wrapper');
+let priceWrapper = document.querySelector('.price_wrapper');
+
+priceWrapper.addEventListener('click', showBasket);
 
 for (let i = 0; i < productCards.length; i++) {
     const productCard = productCards[i];
     productCard.addEventListener('click', showProductsOptions);
 
 }
+let basket = document.querySelector('.summary_basket');
 let products = document.querySelector('.products');
 let choosedProducts = document.querySelector('.choosed_products_wrapper');
+function showBasket() {
+    products.style.transform = 'translate(-200vw)';
+    choosedProducts.style.transform = 'translate(-100vw)';
+    basket.style.transform = 'translate(0)';
 
+}
 function showProductsOptions() {
     console.log('slide');
     products.style.transform = 'translate(-100vw)';
     // main.style.minHeight = '100vh';
     backIcon.style.visibility = 'visible';
     choosedProducts.style.transform = 'translate(0)';
+    basket.style.transform = 'translate(200vw)';
     // products.style.display = 'none';
     // document.querySelector('.swiper').style.display = 'none';
     choosedProducts.style.display = 'flex';
@@ -40,6 +50,8 @@ function showProducts() {
     products.style.display = 'grid';
     // document.querySelector('.swiper').style.display = 'flex';
     backIcon.style.visibility = 'hidden';
+    basket.style.transform = 'translate(200vw)';
+
 
 
 }
